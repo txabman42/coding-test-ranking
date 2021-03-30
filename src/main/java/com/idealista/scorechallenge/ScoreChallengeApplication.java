36dd77e82bd3aa1b57c1212cf583d27c4dd15f28
Xabier <x42.martinez@gmail.com>
2021-03-30 19:22:51 +0200

    Add webflux base project

---
+++ src/main/java/com/idealista/scorechallenge/ScoreChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.idealista.scorechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idealista/scorechallenge/ScoreChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idealista.scorechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
